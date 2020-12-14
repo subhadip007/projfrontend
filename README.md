d4e5c6ee96fd8404ed17e448c8e3a63194f7b9ff
subhadip007 <subhadipadhikary007@gmail.com>
2020-12-14 09:11:00 +0530

    added the signup and signin page

---
+++ README.md
@@ -0,0 +1,10 @@
+ #1.To start the npm script write 'npm start' inside your commnad prompt.
+
+    ##the localhost will run on 3000 as of now
+    ###note:
+    1.in order to access the database you have to also run the backend named 'projbackend' in github
+    2.So run both the folders simultaniously to work
+
+    ##File structure for the Front End file system
+    
+           
---
+++ src/user/Signin.js
@@ -0,0 +1,51 @@
+import React,{useState} from 'react'
+import Base from '../core/Base'
+import {Link} from 'react-router-dom'
+
+
+const Signin=()=>{
+
+
+    const signInForm =()=>{
+
+        return (
+            <div className='row'>
+             
+             <div className='col-md-6 offset-sm-3 text-left'>
+        
+                 <form>
+                    
+                    <div className='form-group'>
+                         <label className='text-light'>email</label>
+                         <input className='form-control' type ='email' />
+                    </div>
+                    <div className='form-group'>
+                         <label className='text-light'>Password</label>
+                         <input className='form-control' type ='password' />
+                    </div>
+                  <button className='btn btn-success btn-block' >Submit</button>
+                 </form>
+             </div>
+        
+            </div>
+        )
+        
+        }
+        
+        
+
+
+
+return(
+    <Base title='signin page' descriptions='page for sigin'>
+
+{signInForm()}
+
+    </Base>
+    
+    )
+
+}
+
+
+export default Signin
